03b195cb4b374b03bde3ac460c57016335cef0b8
Ycastle1 <82854483+Ycastle1@users.noreply.github.com>
2025-06-03 00:28:49 +0900

    refactor : 코드 수정 ( utils/connect )
    connectDB -> selectDB / updateDB 로 sql문 별 호출 함수 분기 처리 및 적용

--- utils/connection.py
+++ utils/connection.py
@@ -1,12 +1,18 @@
 import streamlit as st
-import os
+import os, datetime
+from dateutil.relativedelta import relativedelta
 from dotenv import load_dotenv
+
+def getCorrectTime():
+    correct_time = datetime.datetime.now()
+    target_date = correct_time.strftime('%Y-%m-%d')
+    return target_date
 
 # Database에 연걸하는 메소드
 # Parameter : String sql = 불러오고자 하는 Select문 
 # Return : 반환 받은 DataSet을 Dictonary로 파싱 후 반환
 
-def connectDB(sql):
+def selectDB(sql):
     if os.getenv("GITHUB_ACTIONS") != "true":
         load_dotenv()
         
@@ -26,4 +32,32 @@ def connectDB(sql):
     )
 
     df = conn.query(sql, ttl=0)
+    
     return df.to_dict(orient="records")
+
+
+def updateDB(sqlList, paramList):
+    if os.getenv("GITHUB_ACTIONS") != "true":
+        load_dotenv()
+        
+    conn = st.connection(
+        "sql",
+        dialect="mysql",
+        driver="pymysql",
+        host=os.getenv("ODB_HOST"),
+        database=os.getenv("DB_NAME"),
+        username=os.getenv("ODB_USER"),
+        password=os.getenv("ODB_PASSWORD"),
+        query={
+        # "driver": "MySQL ODBC 8.0 ANSI Driver",  # 또는 "MySQL ODBC 8.0 Unicode Driver"
+        "charset": "utf8mb4",
+        # "ssl-mode": "REQUIRED",  # (필요한 경우)
+        },
+    )
+
+    with conn.session as session:
+        for i in range(0, len(sqlList)):
+            print(i)
+            session.execute(sqlList[i], paramList[i])
+            session.commit()
+    return {"status": "success"}
